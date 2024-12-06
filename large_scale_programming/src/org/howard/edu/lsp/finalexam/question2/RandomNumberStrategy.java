54eb046f815d86df1f1e33cad56e6bbc10b76604
f-ekundayo <obafemi571@gmail.com>
2024-12-05 19:45:27 -0500

    Created RandomNumberStrategy interface to define a pluggable random number generation contract

---
+++ large_scale_programming/src/org/howard/edu/lsp/finalexam/question2/RandomNumberStrategy.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.finalexam.question2;
+
+public interface RandomNumberStrategy {
+    /**
+     * Generates a random positive integer.
+     *
+     * @return a positive integer
+     */
+    int generateRandomNumber();
+}
