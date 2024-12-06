917008e98f4dccc75e4d1806b98343cc66746f36
f-ekundayo <obafemi571@gmail.com>
2024-12-05 19:38:12 -0500

    Testing for GCD

--- large_scale_programming/src/org/howard/edu/lsp/finalexam/question1/MathUtilsTest.java
+++ large_scale_programming/src/org/howard/edu/lsp/finalexam/question1/MathUtilsTest.java
@@ -69,5 +69,41 @@ public class MathUtilsTest {
         assertFalse(mathUtils.isPrime(1),
                 "isPrime(1) should return false");
     }
+
+
+    // ----------------------
+    // Tests for gcd()
+    // ----------------------
+
+    @Test
+    void testGcdOfTwoPositiveNumbersShouldReturnCorrectResult() {
+        // GCD of 54 and 24 is 6
+        assertEquals(6, mathUtils.gcd(54, 24),
+                "gcd(54,24) should return 6");
+    }
+
+    @Test
+    void testGcdWhenOneNumberIsZeroShouldReturnAbsoluteValueOfOther() {
+        // gcd(0, 5) = 5 and gcd(-7, 0) = 7
+        assertEquals(5, mathUtils.gcd(0, 5),
+                "gcd(0,5) should return 5");
+        assertEquals(7, mathUtils.gcd(-7, 0),
+                "gcd(-7,0) should return 7");
+    }
+
+    @Test
+    void testGcdWhenBothNumbersAreZeroShouldThrowException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> mathUtils.gcd(0, 0),
+                "gcd(0,0) should throw IllegalArgumentException");
+    }
+
+    // Optional additional test: gcd with negative numbers ensures values are taken as absolute
+    @Test
+    void testGcdWithNegativeNumbersShouldReturnCorrectResult() {
+        // gcd(-12, -8) = 4
+        assertEquals(4, mathUtils.gcd(-12, -8),
+                "gcd(-12,-8) should return 4");
+    }
 }
 
